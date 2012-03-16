b318ab77686293b56171104cb977f67f815cffa5
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2012-03-16 15:35:27 -0300

    //now when a resource can be upgraded it's shown on the village out map

--- Scripts/_ContentScript.js
+++ Scripts/_ContentScript.js
@@ -232,6 +232,7 @@ function pageProcessAll(info) {
         globalOverflowTimer();
 
     if (where === "Build") globalInBuild();
+    else if (where == "VillageOut") globalInVillageOut();
     else if (where === "SendTroops") globalInSendTroops();
     else if (where === "Reports") globalInReports(info.search);
     else return;
@@ -433,6 +434,45 @@ function globalGetWarehousMax(index) {
  */
 function globalGetWarehousInfo(index) {
     return $("#l" + (index)).text();
+}
+
+/**
+ * Calls all Village Out related functions.
+ *
+ * @author Ignacio Munizaga
+ */
+function globalInVillageOut() {
+    if (dev) console.log("globalInVillageOut() - In Village Out calls...");
+
+    if (checkBuildBuildingResourceDifference === "On" | checkBuildBuildingResourceDifference === "null") {
+        $("#village_map").children().each(function(index) {
+            for (var resource_index = 0; resource_index < 4; resource_index++) {
+                if ($.inArray(index, Enums.VillageMaps[village.VillageOut.type][resource_index]) != -1){
+                    level = parseInt($(this).html());
+                    next_level = "lvl" + (level + 1);
+                    enough_resources = true;
+                    for (var cost_index = 1; cost_index < 5; cost_index++) {
+                        var current_cost = Enums.Fields[resource_index][next_level][cost_index];
+                        var current_ammount = globalGetWarehousAmount(cost_index);
+                        if (current_cost > current_ammount){
+                            enough_resources = false;
+                            break;
+                        }
+                    }
+                    if (enough_resources){
+                        $(this).css("font-size", "20px");
+                        $(this).css("color", "#ffffff");
+                        $(this).css("background-color", "#AAFF55");
+                        $(this).css("padding", "#AAFF55");
+                        $(this).css("border-radius", "20px");
+                        $(this).animate({top: "+=-2"});
+                    }
+                    break;
+                }
+            }
+        });
+    }
+    if (dev) console.log("globalInVillageOut() - In Village out finished successfully!");
 }
 
 /**
