c3d97f6e70de3ce731051475c5974d3416c9949c
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2012-11-25 18:00:12 +0100

    Google Analytics for Plugins

--- Plugins/Core/Services/Services.js
+++ Plugins/Core/Services/Services.js
@@ -11,6 +11,18 @@
  *
  *****************************************************************************/
 
+// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33221456-3']);
+_gaq.push(['_trackEvent', 'Plugin', 'Core/Services']);
+
+(function () {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 // <summary>
 // Plugin that takes care of all built-in models (fill and update), refreshes page and changes views randomly.
 // </summary>
--- Plugins/Utility/VillageListEnhancement/VillageListEnhancement.js
+++ Plugins/Utility/VillageListEnhancement/VillageListEnhancement.js
@@ -9,6 +9,18 @@
  *
  *****************************************************************************/
 
+// Google analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33221456-3']);
+_gaq.push(['_trackEvent', 'Plugin', 'Utility/VillageListEnhancement']);
+
+(function () {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = 'https://ssl.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
 function VillageListEnhancement() {
 	/// <summary>
 	/// Initializes object 
