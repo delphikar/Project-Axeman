5985a6d09694c12d7c707e03fb0dd6b881532957
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-08-02 01:21:01 +0200

    Plugin design

--- Pages/Options/OptionsScript.js
+++ Pages/Options/OptionsScript.js
@@ -1,36 +1,124 @@
-﻿$(document).ready(function () {
-	// Go through all plugins in global list and add them to the page
-	$.each(GlobalPluginsList, function (index, obj) {
-		// Skip internal plugins if not in development mode
-		if (obj.Flags.Internal && !IsDevelopmentMode) {
-			console.warn("PluginsManagerPage: Internal plugin [" + obj.Name + "] ");
+﻿function Options() {
+	this.InitializeSettings = function() {
+		//
+		// Extension State
+		//
+		var extensionStateCheckbox = $("#SettingsExtensionStateCheckbox");
+		var extensionStateMessage = $("#SettingsExtensionStateDisabledMessage");
+		extensionStateCheckbox.change(function() {
+			var isChecked = !extensionStateCheckbox.prop('checked');
+
+			// Handle Disabled message
+			if (!isChecked) {
+				extensionStateMessage.hide();
+			} else {
+				extensionStateMessage.show();
+			}
+		});
+	};
+
+	this.InitializePlugins = function() {
+		//
+		// Plugins
+		//
+		var PluginsViewModel = function(plugins) {
+			this.plugins = ko.observableArray(plugins);
+		};
+
+		var pluginsArray = new Array();
+
+		// Go through all plugins in global list and add them to the page
+		$.each(GlobalPluginsList, function (index, metadata) {
+			// Skip internal plugins if not in development mode
+			if (metadata.Flags.Internal && !IsDevelopmentMode) {
+				return true;
+			}
+
+			metadata["ImageSource"] = GetPluginImage(metadata);
+			metadata["State"] = GetActiveState(metadata);
+			pluginsArray.push(metadata);
 			return true;
+		});
+
+		ko.applyBindings(new PluginsViewModel(pluginsArray));
+	};
+
+	function GetActiveState(obj) {
+		var activeState = null;
+
+		// Gets currently set plugin state
+		var stateObject = JSON.parse(localStorage.getItem("IsPluginActive" + obj.Name));
+
+		// If satte is not set, save default state else 
+		if (stateObject === null) {
+			activeState = obj.Default.State;
+			localStorage.setItem("IsPluginActive" + obj.Name, JSON.stringify({ State: activeState }));
 		}
+		else activeState = stateObject.State;
 
-		// Append plugin item element and add actions to it
-		DrawTable(obj);
-		ActionTable(obj);
+		return activeState;
+	};
+};
 
-		return true;
-	});
-
-	// Google Analytics
-	if (!IsDevelopmentMode) {
-		// Initialize GA
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-33221456-3']);
-		_gaq.push(['_trackPageview']);
-
-		(function () {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = 'https://ssl.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-	}
+$(document).ready(function () {
+	// Instantiate options and initialize
+	var optionsInstance = new Options();
+	optionsInstance.InitializeSettings();
+	optionsInstance.InitializePlugins();
 });
 
-function DrawTable(obj) {
-};
+//
+// chrome-bootstrap
+//
+$(function () {
+	$('.menu a').click(function (ev) {
+		ev.preventDefault();
+		var selected = 'selected';
 
-function ActionTable(obj) {
-};
+		$('.mainview > *').removeClass(selected);
+		$('.menu li').removeClass(selected);
+		setTimeout(function () {
+			$('.mainview > *:not(.selected)').css('display', 'none');
+		}, 100);
+
+		// Rename title to selected item
+		$("title").text($(ev.currentTarget).text());
+
+		$(ev.currentTarget).parent().addClass(selected);
+		var currentView = $($(ev.currentTarget).attr('href'));
+		currentView.css('display', 'block');
+		setTimeout(function () {
+			currentView.addClass(selected);
+		}, 0);
+
+		setTimeout(function () {
+			$('body')[0].scrollTop = 0;
+		}, 200);
+	});
+
+	$('#launch_modal').click(function (ev) {
+		ev.preventDefault();
+		var modal = $('.overlay').clone();
+		$(modal).removeAttr('style');
+		$(modal).find('button, .close-button').click(function () {
+			$(modal).addClass('transparent');
+			setTimeout(function () {
+				$(modal).remove();
+			}, 1000);
+		});
+
+		$(modal).click(function () {
+			$(modal).find('.page').addClass('pulse');
+			$(modal).find('.page').on('webkitAnimationEnd', function () {
+				$(this).removeClass('pulse');
+			});
+		});
+		$(modal).find('.page').click(function (ev) {
+			ev.stopPropagation();
+		});
+		$('body').append(modal);
+	});
+
+	$('.mainview > *:not(.selected)').css('display', 'none');
+});
+
